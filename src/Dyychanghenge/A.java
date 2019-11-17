5b1e888ad6d3226a95f244c4e00bdf941a3eed6d
DongYou1029 <57861492+DongYou1029@users.noreply.github.com>
2019-11-17 21:24:52 +0800

    Add files via upload

---
+++ src/Dyychanghenge/A.java
@@ -0,0 +1,13 @@
+package Dyychanghenge;
+
+public class A extends Exception {
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public A(){
+	 }
+	 public A(String str) {
+	  super (str);
+	 }
+	}
---
+++ src/Dyychanghenge/Test.java
@@ -0,0 +1,24 @@
+package Dyychanghenge;
+
+public class Test { 
+	 public static void main(String args[]) throws A{ 
+	 Changhenge changhege; 
+	 String b; 
+	 b=""; 
+	 try { 
+	 changhege=new Changhenge(b); 
+	 if (b=="") { 
+	 throw new A("不能输入空字符"); 
+	 } 
+	 else { 
+	 System.out.print(changhege); 
+	 } 
+	 } 
+	 catch(A r) { 
+	 r.printStackTrace(); 
+	 } 
+	 finally { 
+	 System.out.print("程序运行结束"); 
+	 } 
+	   } 
+	 } 
